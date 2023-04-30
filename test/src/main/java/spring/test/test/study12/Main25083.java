ca52af8a1bf0be127cbee8ab8ed70bce9f54f558
suminLee <pkpk85@naver.com>
2023-04-30 23:37:42 +0900

    coding test 12

---
+++ test/src/main/java/spring/test/test/study12/Main25083.java
@@ -0,0 +1,21 @@
+package spring.test.test.study12;
+//새싹
+//아래 예제와 같이 새싹을 출력하시오.
+
+//입력
+//입력은 없다.
+
+//출력
+//새싹을 출력한다.
+public class Main25083 {
+    public static void main(String[] args) {
+        String s = "         ,r'\"7\n" + 	// \", \n 이 제어문자다.
+                "r`-_   ,'  ,/\n" + 		// \n 이 제어문자다.
+                " \\. \". L_r'\n" + 		// \\, \", \n 이 제어문자다.
+                "   `~\\/\n" + 			// \\, \n 이 제어문자다.
+                "      |\n" + 			// \n 이 제어문자다.
+                "      |";
+
+        System.out.print(s);
+    }
+}
