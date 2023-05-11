d2acd958b0b3e4f93524b5fd6813f27b8a865248
suminLee <pkpk85@naver.com>
2023-05-11 17:22:48 +0900

    subject 1.3

--- test/src/main/java/spring/test/test/study2/Main10172.java
+++ test/src/main/java/spring/test/test/study2/Main10172.java
@@ -1,5 +1,12 @@
 package spring.test.test.study2;
+//개
+//아래 예제와 같이 개를 출력하시오.
 
+//입력
+//없음.
+
+//출력
+//개를 출력한다.
 public class Main10172 {
     public static void main(String[] args) {
         System.out.println("|\\_/|");
