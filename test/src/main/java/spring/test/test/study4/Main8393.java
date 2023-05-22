08bb48c0764a739c5006c02f5d7f6769683834aa
suminLee <pkpk85@naver.com>
2023-05-23 08:51:08 +0900

    subject 2.3

--- test/src/main/java/spring/test/test/study4/Main8393.java
+++ test/src/main/java/spring/test/test/study4/Main8393.java
@@ -1,4 +1,12 @@
 package spring.test.test.study4;
+//합
+//n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
+
+//입력
+//첫째 줄에 n (1 ≤ n ≤ 10,000)이 주어진다.
+
+//출력
+//1부터 n까지 합을 출력한다.
 import java.util.Scanner;
 public class Main8393 {
     public static void main(String[] args) {
